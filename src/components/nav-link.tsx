7ce4e5d91fdeee4a076ad583e3441faeddacf8fc
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-01-20 01:54:20 -0400

    Páginas e componentes (UI) - Layout do app com cabeçalho

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,19 @@
+import { NavLink as Link, NavLinkProps as LinkProps } from 'react-router-dom'
+
+import { cn } from '@/lib/utils'
+
+type NavLinkProps = LinkProps
+
+export function NavLink(props: NavLinkProps) {
+  return (
+    <Link
+      className={({ isActive }) =>
+        cn(
+          'flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground',
+          isActive && 'text-foreground',
+        )
+      }
+      {...props}
+    />
+  )
+}
