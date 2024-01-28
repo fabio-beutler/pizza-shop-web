a658250987df9d64e2bc24305a2c63f183d58f50
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-01-28 15:22:06 -0400

    Add useGetDayOrdersAmount hook to fetch day orders amount

--- src/api/get-day-orders-amount.ts
+++ src/api/get-day-orders-amount.ts
@@ -1,3 +1,5 @@
+import { useQuery } from '@tanstack/react-query'
+
 import { api } from '@/lib/axios'
 
 export interface GetDayOrdersAmountResponse {
@@ -5,9 +7,16 @@ export interface GetDayOrdersAmountResponse {
   diffFromYesterday: number
 }
 
-export async function getDayOrdersAmount() {
+async function getDayOrdersAmount() {
   const response = await api.get<GetDayOrdersAmountResponse>(
     '/metrics/day-orders-amount',
   )
   return response.data
+}
+
+export function useGetDayOrdersAmount() {
+  return useQuery({
+    queryKey: ['metrics', 'day-orders-amount'],
+    queryFn: getDayOrdersAmount,
+  })
 }
--- src/pages/app/dashboard/components/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/components/day-orders-amount-card.tsx
@@ -1,21 +1,12 @@
-import { useQuery } from '@tanstack/react-query'
 import { Utensils } from 'lucide-react'
 
-import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
+import { useGetDayOrdersAmount } from '@/api/get-day-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 import { MetricCardSkeleton } from './metric-card-skeleton'
 
 export function DayOrdersAmountCard() {
-  const {
-    data: dayOrdersAmount,
-    isLoading,
-    isSuccess,
-    isError,
-  } = useQuery({
-    queryKey: ['metrics', 'day-orders-amount'],
-    queryFn: getDayOrdersAmount,
-  })
+  const getDayOrdersAmountQuery = useGetDayOrdersAmount()
 
   return (
     <Card>
@@ -24,27 +15,26 @@ export function DayOrdersAmountCard() {
         <Utensils className="size-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {isSuccess && (
+        {getDayOrdersAmountQuery.isLoading && <MetricCardSkeleton />}
+        {getDayOrdersAmountQuery.isSuccess && (
           <>
             <span className="text-2xl font-bold tracking-tight">
-              {dayOrdersAmount.amount.toLocaleString('pt-BR')}
+              {getDayOrdersAmountQuery.data.amount.toLocaleString('pt-BR')}
             </span>
             <p className="text-xs text-muted-foreground">
-              {dayOrdersAmount.diffFromYesterday >= 0 ? (
+              {getDayOrdersAmountQuery.data.diffFromYesterday >= 0 ? (
                 <span className="text-emerald-500 dark:text-emerald-400">
-                  +{dayOrdersAmount.diffFromYesterday}%
+                  +{getDayOrdersAmountQuery.data.diffFromYesterday}%
                 </span>
               ) : (
                 <span className="text-rose-500 dark:text-rose-400">
-                  {dayOrdersAmount.diffFromYesterday}%
+                  {getDayOrdersAmountQuery.data.diffFromYesterday}%
                 </span>
               )}{' '}
               em relação ao dia anterior
             </p>
           </>
         )}
-        {isLoading && <MetricCardSkeleton />}
-        {isError && <h3>Erro ao carregar</h3>}
       </CardContent>
     </Card>
   )
