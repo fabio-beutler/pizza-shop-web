fbf0463f1903b152e1706da5f6def0eebad5bcb9
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-03-04 09:47:38 -0400

    Refactor Input component to remove containerClassName prop

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,16 +4,15 @@ import { cn } from '@/lib/utils'
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   errorMessage?: string | null
-  containerClassName?: string
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, containerClassName, errorMessage = null, ...props }, ref) => {
+  ({ className, errorMessage = null, ...props }, ref) => {
     const hasErrorMessage = errorMessage !== null
     const Comp = hasErrorMessage ? 'div' : React.Fragment
 
     return (
-      <Comp className={containerClassName}>
+      <Comp>
         <input
           className={cn(
             'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
